9f16249d1930dd5d31a0e388ca5ba188002c262d
Maryam-bit <rajputmaryam5555@gmail.com>
2022-10-23 08:44:07 -0700

    feat: making HTTP request

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Starting')
+const request = require("request")
+ const url = "http://api.weatherstack.com/current?access_key=05a695312f2b68a694c3fce46bb9b0ec&query=37.8267,-122.4233"
+
+ request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+ })
