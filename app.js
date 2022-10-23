8b1d147165c2f5807ee0110080a479acb8099e0c
Maryam-bit <rajputmaryam5555@gmail.com>
2022-10-23 10:35:03 -0700

    feat: get logitude and latitude using positionstack

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const request = require("request")
- const url = "http://api.weatherstack.com/current?access_key=05a695312f2b68a694c3fce46bb9b0ec&query=37.8267,-122.4233"
+const url = "http://api.weatherstack.com/current?access_key=05a695312f2b68a694c3fce46bb9b0ec&query=37.8267,-122.4233&units=f"
 
- request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
- })
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current);
+    console.log(`${response.body.current.weather_descriptions[0]}. It is current ${response.body.current.temperature} degrees. But it feels  like ${response.body.current.feelslike} degrees`)
+})
+
+//position stack
+// address -> lat/long -> weather
+
+const positionstackURL = "http://api.positionstack.com/v1/forward?access_key=d1736f065c7531db59f6ab856889f07e&query=Los Angeles";
+
+request({ url: positionstackURL, json: true }, (error, response) => {
+    console.log(response.body)
+    const longitude = response.body.data[0].longitude
+    const latitude = response.body.data[0].latitude
+    console.log(longitude, latitude, "longitude latitude")
+})
