006da65844cb435fe60f1bea833ef6182cb78a06
Maryam-bit <rajputmaryam5555@gmail.com>
2022-11-01 12:49:38 -0700

    feat: create endpoint for weather api and get data

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require("express");
 const hbs = require('hbs')
 const app = express();
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast")
 
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -39,10 +41,39 @@ app.get('/help', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'Please provide an address'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            res.send(error)
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send(error)
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            });
+        })
+    })
+})
+
+app.get("/products", (req, res) =>{
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query)
     res.send({
-        forecast: 'It is snowing',
-        location: 'Pakistan'
-    });
+        products: []
+    })
 })
 
 
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require("request")
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=05a695312f2b68a694c3fce46bb9b0ec&query=${latitude},${longitude}&units=f`
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("unable to connect to weather service", undefined);
+        } else if (body.error) {
+            callback("unable to find location", undefined);
+        } else {
+            const tempInCelsius = fToC(body.current.temperature)
+            const feelsLikeTempInCelsius = fToC(body.current.feelslike)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is current ${tempInCelsius} degree. But it feels  like ${feelsLikeTempInCelsius} degree`)
+        }
+    })
+}
+
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+    return fToCel
+} 
+
+module.exports = forecast;
